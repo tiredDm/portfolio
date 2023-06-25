0aa46a7d15ea90e607baa6afc6d6eb449536b775
Rohan Balaji <114772518+tiredDm@users.noreply.github.com>
2023-06-25 09:25:18 -0400

    Map-page basics
    Added Continental Map & it's border card to the map page. Next should be create the pages for the other made maps..

--- portfolio/src/app/map-page/map-page.component.ts
+++ portfolio/src/app/map-page/map-page.component.ts
@@ -2,8 +2,20 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-map-page',
-  template: `<p>map-page works!</p>`,
+  template: `
+  
+  <p>map-page works!</p>
+
+  <div class = "page-layout">
+    <div class = "map-card">
+      <img class = "miria-map" src= "assets/img/maps/Miria.png">
+    </div> 
+  </div>
+  `,
   styles: [
+    '.page-layout{display: flex; align-items:center; justify-content:center}',
+    '.map-card{height:75vh; width: 52vw;  background-color: #3b3832; display: flex; align-items:center; justify-content:center; }',
+    '.miria-map{height: 70vh; width: 50vw;}'
   ]
 })
 export class MapPageComponent {}
