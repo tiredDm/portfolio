475a6cd4ce14e46d753930a45cdc7977aa433a6b
Rohan Balaji <114772518+tiredDm@users.noreply.github.com>
2023-06-25 13:23:16 -0400

    Map button Layout Placement.
    Figured out the CSS to get the "button" within the image I want. To get the links working right I'll need to use child routes I think.
    
    (working on that first is key).
    (then implementing tool tips, from there it's just grinding it out)

--- portfolio/src/app/map-page/map-page.component.ts
+++ portfolio/src/app/map-page/map-page.component.ts
@@ -8,14 +8,20 @@ import { Component } from '@angular/core';
 
   <div class = "page-layout">
     <div class = "map-card">
-      <img class = "miria-map" src= "assets/img/maps/Miria.png">
+      <div class = "map-layout">
+        <img class = "miria-map" src= "assets/img/maps/Miria.png">
+        <div class = "drogheda-button"> == </div> 
+
+      </div>
     </div> 
   </div>
   `,
   styles: [
     '.page-layout{display: flex; align-items:center; justify-content:center}',
     '.map-card{height:75vh; width: 52vw;  background-color: #3b3832; display: flex; align-items:center; justify-content:center; }',
-    '.miria-map{height: 70vh; width: 50vw;}'
+    '.map-layout{height: 70vh; width: 44vw; position: relative}',
+    '.miria-map{height: 100%; width: 100%;}', 
+    '.drogheda-button{position: absolute; left: 22%; top: 27%;background-color: #bbb; color: #bbb; border-radius: 50%; border: 0px solid #49b51f}',
   ]
 })
 export class MapPageComponent {}
