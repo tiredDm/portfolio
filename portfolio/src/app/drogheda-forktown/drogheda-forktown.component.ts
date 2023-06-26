77242ba555c826a50b1eaa3b7f1c3ea0e41932d7
Rohan Balaji <114772518+tiredDm@users.noreply.github.com>
2023-06-26 04:42:06 -0400

    maps
    All drogheda maps added.

--- portfolio/src/app/drogheda-forktown/drogheda-forktown.component.ts
+++ portfolio/src/app/drogheda-forktown/drogheda-forktown.component.ts
@@ -2,8 +2,25 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-drogheda-forktown',
-  template: `<p>drogheda-forktown works!</p>`,
+  template: `
+  <p>drogheda-forktown works!</p>
+
+  <div class = "page-layout">
+    <div class = "map-card">
+      <div class = "map-layout">
+        <img class = "map" src= "assets/img/maps/Forktown-map.png">
+        <div class = "drogheda-button"> == </div> 
+      </div>
+    </div> 
+  </div>
+
+  `,
   styles: [
+    '.page-layout{display: grid; place-items: center}',
+    '.map-card{height:75vh; width: 52vw;  background-color: #3b3832; display: flex; align-items:center; justify-content:center; }',
+    '.map-layout{height: 70vh; width: 44vw; position: relative}',
+    '.map{height: 100%; width: 100%;}', 
+    '.drogheda-button{position: absolute; left: 22%; top: 27%;background-color: #bbb; color: #bbb; border-radius: 50%; border: 0px solid #49b51f}',
   ]
 })
 export class DroghedaForktownComponent {}
