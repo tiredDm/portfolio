1f2fb5f9987dc2874721b3f98c6784e42eb90adf
Rohan Balaji <114772518+tiredDm@users.noreply.github.com>
2023-06-27 04:15:27 -0400

    removed buttons
    Removed unnecessary buttons in all the maps that didn't need it.

--- portfolio/src/app/drogheda-forktown/drogheda-forktown.component.ts
+++ portfolio/src/app/drogheda-forktown/drogheda-forktown.component.ts
@@ -9,7 +9,6 @@ import { Component } from '@angular/core';
     <div class = "map-card">
       <div class = "map-layout">
         <img class = "map" src= "assets/img/maps/Forktown-map.png">
-        <div class = "drogheda-button"> == </div> 
       </div>
     </div> 
   </div>
@@ -20,7 +19,6 @@ import { Component } from '@angular/core';
     '.map-card{height:75vh; width: 52vw;  background-color: #3b3832; display: flex; align-items:center; justify-content:center; }',
     '.map-layout{height: 70vh; width: 44vw; position: relative}',
     '.map{height: 100%; width: 100%;}', 
-    '.drogheda-button{position: absolute; left: 22%; top: 27%;background-color: #bbb; color: #bbb; border-radius: 50%; border: 0px solid #49b51f}',
   ]
 })
 export class DroghedaForktownComponent {}
